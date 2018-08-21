b108e6db3549809f72a4371be87cbb5aae0ca24e
alexsantosfilho <devalexsandro@gmail.com>
2018-08-21 14:40:37 -0300

    up 2

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'ddtrace'
+#gem 'ddtrace', :git => 'git://github.com/alexsantosfilho/ruby.git'
 git_source(:github) { |repo| "https://github.com/alexsantosfilho/ruby.git" }
 
 ruby '2.5.0'
--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -3,3 +3,22 @@
 Datadog.configure do |c|
     c.use :rails, service_name: 'my-rails-app'
   end
+  
+
+Rails.configuration.datadog_trace = {
+    enabled: true,
+    auto_instrument: true,
+    auto_instrument_redis: true,
+    auto_instrument_grape: true,
+    default_service: 'my-rails-app',
+    default_controller_service: 'my-rails-controller',
+    default_cache_service: 'my-rails-cache',
+    default_database_service: 'postgresql',
+    template_base_path: 'views/',
+    tracer: Datadog.tracer,
+    debug: false,
+    trace_agent_hostname: 'datadog',
+    trace_agent_port: 8126,
+    env: nil,
+    tags: {}
+  }
